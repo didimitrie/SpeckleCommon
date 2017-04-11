bda11b60f41780f870cb532a0ddab487eab47b90
didimitrie <didimitrie@gmail.com>
2017-04-11 18:57:26 +0100

    Fiddle.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpeckleClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SpeckleCommon")]
+[assembly: AssemblyDescription(".NET base for Speckle Clients")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SpeckleClient")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("SpeckleCommon")]
+[assembly: AssemblyCopyright("Copyright Dimitrie A. Stefanescu, UCL The Bartlett & InnoChain Network Members © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// PLEASE NOTE WE ARE ACTUALLY USING SEMVER
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
