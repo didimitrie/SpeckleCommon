f635c0e8486a7666c097081aad14dc518f1c4d2f
didimitrie <didimitrie@gmail.com>
2017-05-05 17:25:07 +0200

    Split up utlis.cs into more, started work on SpeckleDataTypes

---
+++ SpeckleConverter.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeckleCommon
+{
+    /// <summary>
+    /// Standardises object conversion. The SpeckleApiProxy requires a converter object which should derive from this interface.
+    /// </summary>
+    [Serializable]
+    public abstract class SpeckleConverter
+    {
+        public SpeckleConverter()
+        {
+        }
+
+        #region global convert functions
+
+        /// <summary>
+        /// Converts a list of objects. 
+        /// </summary>
+        /// <param name="objects">Objects to convert.</param>
+        /// <param name="getEncoded">If true, should return a base64 encoded version of the object as well.</param>
+        /// <param name="getAbstract">If set to false will not return speckle-parsable values for objects.</param>
+        /// <returns>A list of dynamic objects.</returns>
+        abstract public List<SpeckleObject> convert(IEnumerable<object> objects);
+
+        /// <summary>
+        /// Async conversion of a list of objects. Returns the result in a callback.
+        /// </summary>
+        /// <param name="objects">Objects to convert.</param>
+        /// <param name="callback">Action to perform with the converted objects.</param>
+        abstract public void convertAsync(IEnumerable<object> objects, Action<List<SpeckleObject>> callback);
+
+
+        abstract public List<SpeckleObjectProperties> getObjectProperties(IEnumerable<object> objects);
+
+        #endregion
+
+        /// <summary>
+        /// Encodes an object back to its native type.
+        /// </summary>
+        /// <param name="myObj">object to encode to native.</param>
+        /// <returns></returns>
+        abstract public object encodeObject(dynamic myObj, dynamic objectProperties = null);
+
+        #region standard types: boolean, numbers, strings
+
+        public static SpeckleObject fromBoolean(bool b)
+        {
+            return new SpeckleBoolean(b);
+        }
+
+        public static bool toBoolean(dynamic b)
+        {
+            return b.value;
+        }
+
+        public static SpeckleObject fromNumber(double num)
+        {
+            return new SpeckleNumber(num);
+        }
+
+        public static double toNumber(dynamic num)
+        {
+            return (double)num.value;
+        }
+
+        public static SpeckleObject fromString(string str)
+        {
+            return new SpeckleString(str);
+        }
+
+        public static string toString(dynamic str)
+        {
+            return (string)str.value;
+        }
+
+        #endregion
+
+        #region utils
+
+        /// <summary>
+        /// Encodes a serialisable object in base64 string.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string getBase64(object obj)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter().Serialize(ms, obj);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Encodes a serialisable object in a byte array.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static byte[] getBytes(object obj)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter().Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Hashes a given string using md5. Used to get object hashes. 
+        /// </summary>
+        /// <param name="str">What to hash.</param>
+        /// <returns>a lowercase string of the md5 hash.</returns>
+        public static string getHash(string str)
+        {
+            byte[] hash;
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                hash = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hash)
+                    sb.Append(b.ToString("X2"));
+
+                return sb.ToString().ToLower();
+            }
+        }
+
+        public static object getObjFromString(string base64String)
+        {
+            if (base64String == null) return null;
+            byte[] bytes = Convert.FromBase64String(base64String);
+            return getObjFromBytes(bytes);
+        }
+
+        public static object getObjFromBytes( byte[] bytes )
+        {
+            using (MemoryStream ms = new MemoryStream(bytes, 0, bytes.Length))
+            {
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Position = 0;
+                return new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter().Deserialize(ms);
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Spits out a string descriptor, ie "grasshopper-converter", or "rhino-converter". Used for reiniatisation of the API Proxy.
+        /// </summary>
+        /// <returns></returns>
+        abstract public dynamic description();
+    }
+}
---
+++ SpeckleObjects.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeckleCommon
+{
+    [Serializable]
+    public class SpeckleObject 
+    {
+        public string type;
+    }
+
+    public class SpeckleBoolean : SpeckleObject
+    {
+        public bool value;
+        public SpeckleBoolean(bool val)
+        {
+            type = "Boolean";
+            value = val;
+        }
+    }
+
+    public class SpeckleNumber : SpeckleObject
+    {
+        public double value;
+        public SpeckleNumber(double val)
+        {
+            type = "Number";
+            value = val;
+        }
+    }
+
+    public class SpeckleString: SpeckleObject
+    {
+        public string value;
+        public SpeckleString(string val)
+        {
+            type = "String";
+            value = val;
+        }
+    }
+
+    public class SpeckleInterval : SpeckleObject
+    {
+        public double[] value = new double[2];
+        public SpeckleInterval(double start, double end)
+        {
+            type = "Interval";
+            value[0] = start; value[1] = end;
+        }
+    }
+
+    public class SpeckleInterval2d : SpeckleObject
+    {
+        public double[] value = new double[4];
+        public SpeckleInterval2d(double s1, double e1, double s2, double e2)
+        {
+            type = "Interval2d";
+            value[0] = s1; value[1] = e1;
+            value[2] = s2; value[3] = e2;
+        }
+    }
+
+    public class SpecklePoint : SpeckleObject
+    {
+        public double[] value = new double[3];
+        public SpecklePoint(double x, double y, double z)
+        {
+            type = "Point";
+            value[0] = x; value[1] = y; value[2] = z;
+        }
+    }
+
+
+    public class SpeckleVector : SpeckleObject
+    {
+        public double[] value = new double[3];
+        public SpeckleVector(double x, double y, double z)
+        {
+            type = "Vector";
+            value[0] = x; value[1] = y; value[2] = z;
+        }
+    }
+
+    public class SpecklePlane : SpeckleObject
+    {
+        dynamic value;
+
+        public SpecklePlane( SpecklePoint origin, SpeckleVector normal, SpeckleVector xdir, SpeckleVector ydir )
+        {
+            type = "Plane";
+            value = new ExpandoObject();
+            value.origin = origin.value;
+            value.normal = normal.value;
+            value.xdir = xdir.value;
+            value.ydir = ydir.value;
+        }
+    }
+}
